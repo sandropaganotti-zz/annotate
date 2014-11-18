6453dbb24a1533637a658e62b961ee178289e737
gabriele.lana <gabriele.lana@cleancode.it>
2014-11-18 23:00:26 +0100

    Identify hostname from primus request extracted as primus plugin

--- server/app.js
+++ server/app.js
@@ -11,10 +11,18 @@ var server = require('http').createServer(app);
 var primus = new Primus(server, {transformer: 'websockets'});
 
 primus.use('rooms', require('primus-rooms'));
+
+primus.use('hostname', {
+  server: function(primus, options) {
+    primus.on('connection', function(spark) {
+      spark.hostname = spark.request.headers.host.split(':')[0];
+    });
+  }
+});
+
 primus.on('connection', function(spark) {
-  var domain = spark.request.headers.host.split(':')[0];
-  console.log('connected', spark.id, '@domain', domain);
-  spark.join(domain);
+  console.log('connected', spark.id, '@domain', spark.hostname);
+  spark.join(spark.hostname);
 });
 
 primus.on('disconnection', function(spark) {
