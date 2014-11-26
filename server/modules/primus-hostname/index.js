c125ca55de53bb89afa329d82b81c4e0d6e0fca0
gabriele.lana <gabriele.lana@cleancode.it>
2014-11-26 18:09:04 +0100

    Fix domain detection in ws request

--- server/modules/primus-hostname/index.js
+++ server/modules/primus-hostname/index.js
@@ -1,7 +1,11 @@
 module.exports = {
   server: function(primus, options) {
+    var url = require('url');
     primus.on('connection', function(spark) {
-      spark.hostname = spark.request.headers.host.split(':')[0];
+      spark.hostname = url.parse(
+        spark.request.headers.origin ||
+        spark.request.headers.host
+      ).hostname;
     });
   }
 };
